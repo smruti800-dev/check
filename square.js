96699f7f5382d166d0e3ef45b9f5328348949be3
Smruti <70853778+smruti800-dev@users.noreply.github.com>
2021-01-21 17:07:55 +0530

    Update square.js

--- square.js
+++ square.js
@@ -9,3 +9,4 @@ const result3 = Math.sqrt(number3);
 console.log(`The square root of ${number1} is ${result1}`);
 console.log(`The square root of ${number2} is ${result2}`);
 console.log(`The square root of ${number3} is ${result3}`);
+console.log(`close`);
